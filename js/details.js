577fbc215f1f24c34e64d2a0bd53ee6e480c2897
Tina <tina.bekkholt@hotmail.com>
2023-02-23 12:33:01 +0100

    more styling

--- js/details.js
+++ js/details.js
@@ -25,7 +25,7 @@ fetchDetails();
 
 function createHtml(details) {
     detailContainer.innerHTML = `<h2>${details.name}</h2>
-    <h5>House founder:</h5><div class="details founder"> ${details.founder}</div>
-    <h5>House ghost:</h5><div class="details ghost"> ${details.ghost}</div>
-    <h5>House commonroom:</h5><div class="details room"> ${details.commonRoom}</div>`;
+    <h5>House founder:</h5><div class="details founder">${details.founder}</div>
+    <h5>House ghost:</h5><div class="details ghost">${details.ghost}</div>
+    <h5>House commonroom:</h5><div class="details room">${details.commonRoom}</div>`;
     }
